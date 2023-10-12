ed35d919169b74d74e4f5239c403403cf3a3724c
EsinShadrach <esinshadrachrafe@gmail.com>
2023-10-12 12:34:45 +0100

    Done with featured item

--- app/resturant/render-featured-items.tsx
+++ app/resturant/render-featured-items.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
+import BgText from "~/utils/bg-text";
 import { FeaturedItemCard } from "~/utils/featured-card";
+import PopularItemCard from "~/utils/popular-item-card";
 
 export default function RenderFeaturedItems() {
   const [featuredItem, setFeaturedItem] = useState<FoodItem[] | null>(null);
+  const [popularItem, setPopularItem] = useState<FoodItem[] | null>(null);
+
   useEffect(() => {
     async function getFeaturedItem() {
       try {
@@ -11,6 +15,8 @@ export default function RenderFeaturedItems() {
         if (response.ok) {
           const responseData: FoodItem[] = await response.json();
           setFeaturedItem(responseData);
+          const popular = responseData.filter((item) => item.customerFavorite);
+          setPopularItem(popular);
         }
       } catch (error) {
         const errorM = error as Error;
@@ -20,11 +26,22 @@ export default function RenderFeaturedItems() {
     getFeaturedItem();
   }, []);
   if (!featuredItem) return <></>;
+
   return (
-    <div className="flex p-10 px-0 pt-0 overflow-x-auto gap-6 scrollbar-none">
-      {featuredItem.map((item, index) => (
-        <FeaturedItemCard {...item} key={index} />
-      ))}
-    </div>
+    <>
+      <div className="flex p-10 px-0 pt-0 overflow-x-auto gap-6 scrollbar-none">
+        {featuredItem.map((item, index) => (
+          <FeaturedItemCard {...item} key={index} />
+        ))}
+      </div>
+      <div className="mt-10">
+        <BgText className="text-lg font-semibold sm:text-xl">
+          Popular Items
+        </BgText>
+        <div>
+          <PopularItemCard />
+        </div>
+      </div>
+    </>
   );
 }
--- utils/featured-card.tsx
+++ utils/featured-card.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import sampleImg from "~/public/sample-food.png";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import BgBackground from "~/utils/bg-bg";
 import BgText from "~/utils/bg-text";
 import ClockIcon from "~/utils/icons/clock";
 import { HeartIcon } from "~/utils/icons/heart-active";
 import StarIcon from "~/utils/icons/star";
 import { PrimaryBg } from "~/utils/primary-bg";
-
 export function FeaturedItemCard({
   categories,
   customerFavorite,
@@ -17,50 +17,56 @@ export function FeaturedItemCard({
   rating,
   ratingCount,
 }: FoodItem) {
+  const pathame = usePathname();
+  const redirectPath = `${pathame}/${id}`;
   return (
-    <div className="flex-shrink-0 w-full max-w-md overflow-hidden shadow-xl rounded-xl">
-      <div className="relative">
-        <div className="absolute inset-0 flex items-start justify-between p-3 bg-black/20">
-          <BgBackground className="flex items-center p-1 px-2 rounded-full">
-            <BgText className="text-sm font-semibold">{rating}</BgText>
-            <StarIcon className="w-6 h-6" />
-            <BgText className="text-xs opacity-50">({ratingCount - 1}+)</BgText>
-          </BgBackground>
-          {customerFavorite && (
-            <PrimaryBg className="flex items-center justify-center p-1 text-white rounded-full">
-              <HeartIcon className="w-5 h-5" />
-            </PrimaryBg>
-          )}
+    <div className="flex-shrink-0 w-full max-w-md overflow-hidden shadow-lg rounded-xl hover:shadow-xl transiton-all duration-300">
+      <Link href={redirectPath}>
+        <div className="relative">
+          <div className="absolute inset-0 flex items-start justify-between p-3 bg-black/20">
+            <BgBackground className="flex items-center p-1 px-2 rounded-full">
+              <BgText className="text-sm font-semibold">{rating}</BgText>
+              <StarIcon className="w-6 h-6" />
+              <BgText className="text-xs opacity-50">
+                ({ratingCount - 1}+)
+              </BgText>
+            </BgBackground>
+            {customerFavorite && (
+              <PrimaryBg className="flex items-center justify-center p-1 text-white rounded-full">
+                <HeartIcon className="w-5 h-5" />
+              </PrimaryBg>
+            )}
+          </div>
+          <Image
+            src={image}
+            alt={name}
+            width={448}
+            height={240}
+            className="object-cover h-60"
+          />
         </div>
-        <Image
-          src={image}
-          alt={name}
-          width={448}
-          height={240}
-          className="object-cover h-60"
-        />
-      </div>
-      <div className="p-3 space-y-0.5">
-        <BgText className="font-semibold opacity-90">{name}</BgText>
-        <div className="flex items-center gap-1">
-          <ClockIcon className="w-4 h-4" />
-          <BgText className="opacity-75">
-            <small>{prepareTime}</small>
-          </BgText>
+        <div className="p-3 space-y-0.5">
+          <BgText className="font-semibold opacity-90">{name}</BgText>
+          <div className="flex items-center gap-1">
+            <ClockIcon className="w-4 h-4" />
+            <BgText className="opacity-75">
+              <small>{prepareTime}</small>
+            </BgText>
+          </div>
+          <div className="flex items-center pt-2 gap-3">
+            {categories.map((category, index) => (
+              <ProductOptionPills key={index} text={`${category}`} />
+            ))}
+          </div>
         </div>
-        <div className="flex items-center pt-2 gap-3">
-          {categories.map((category, index) => (
-            <ProductOptionPills key={index} text={`${category}`} />
-          ))}
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
 
 function ProductOptionPills({ text }: { text: string }) {
   return (
-    <div className="text-gray-400 bg-gray-200 rounded-md text-center max-w-[4rem] overflow-hidden p-2 text-xs text-ellipsis">
+    <div className="text-gray-400 bg-gray-200 rounded-md text-center max-w-[8rem] overflow-hidden p-2 text-xs text-ellipsis whitespace-nowrap">
       {text}
     </div>
   );
