84ec2152d931cf3e3ce28d9fcc2f0402c8c3b00b
EsinShadrach <esinshadrachrafe@gmail.com>
2023-10-14 14:04:17 +0100

    ment page

--- app/resturant/[id]/menu/page.tsx
+++ app/resturant/[id]/menu/page.tsx
@@ -1,4 +1,34 @@
+import Image from "next/image";
+import largePizza from "~/public/large-pizza.png";
+import BgText from "~/utils/bg-text";
 //TODO: Render all the food items here
-export default function Apple({ params, searchParams }: BaseProps) {
-  return <div>apple</div>;
+// ! GET NUMBER OF ITEMS HERE
+// ! GET NAME OF RESTURANT HERE
+export default function Menu({ params, searchParams }: BaseProps) {
+  return (
+    <section className="container p-3 pt-0 pr-0 mx-auto overflow-x-hidden">
+      <div className="flex items-baseline">
+        <div className="pb-20 mt-auto">
+          <h1 className="text-2xl font-semibold leading-tight sm:text-5xl">
+            <BgText className="text-opacity-90">Resturant {params.id}</BgText>
+            <div>Menu</div>
+          </h1>
+          <small>
+            <BgText className="text-opacity-70">80 items in menu</BgText>
+          </small>
+        </div>
+        <Image
+          alt="A pizza"
+          src={largePizza}
+          className="w-full max-w-sm ml-auto"
+          draggable={"false"}
+        />
+      </div>
+      <div className="-mt-12">
+        <div>
+          <small><BgText className="text-opacity-90">Sort by:</BgText></small>
+        </div>
+      </div>
+    </section>
+  );
 }
