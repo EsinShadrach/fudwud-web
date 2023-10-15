85bcb33779d350afb11b0e4112d1aa67b8a59035
EsinShadrach <esinshadrachrafe@gmail.com>
2023-10-15 19:00:12 +0100

    Back button

---
+++ app/resturant/[id]/back-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+import { useTableData } from "~/context/use-table";
+import { PrimaryBg } from "~/utils/primary-bg";
+
+export function BackButton() {
+  const { table, resturantID } = useTableData();
+  const path = `/resturant/${resturantID}/?table=${table}`;
+
+  return (
+    <Link href={path} className="self-start m-3">
+      <PrimaryBg className="flex items-center justify-center p-3 rounded-lg shadow-lg w-9 h-9 bg-opacity-0 hover:bg-opacity-5 transition-all duration-300">
+        &lt;-
+      </PrimaryBg>
+    </Link>
+  );
+}
--- app/resturant/[id]/menu/page.tsx
+++ app/resturant/[id]/menu/page.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import largePizza from "~/public/large-pizza.png";
 import BgText from "~/utils/bg-text";
 import FilterButton from "../../filter-button";
-import { DropDownButton } from "./DropDownButton";
-import { NumberOfItemsInMenu } from "./NumberOfItemsInMenu";
-import { RenderMenu } from "./RenderMenu";
-//TODO: Render all the food items here
-// ! GET NUMBER OF ITEMS HERE
+import {BackButton} from "../back-button";
+import {DropDownButton} from "./DropDownButton";
+import {NumberOfItemsInMenu} from "./NumberOfItemsInMenu";
+import {RenderMenu} from "./RenderMenu";
 // ! GET NAME OF RESTURANT HERE
 export default function Menu({ params, searchParams }: BaseProps) {
   return (
     <section className="container max-h-screen p-3 pt-0 pr-0 mx-auto overflow-auto">
       <div className="flex items-baseline overflow-x-clip">
+        <BackButton />
         <div className="pb-20 mt-auto">
           <h1 className="text-2xl font-semibold leading-tight sm:text-5xl">
             <BgText className="text-opacity-90">Resturant {params.id}</BgText>
@@ -43,3 +43,5 @@ export default function Menu({ params, searchParams }: BaseProps) {
     </section>
   );
 }
+
+
