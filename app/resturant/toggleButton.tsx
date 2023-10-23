4e97263b9b2b6315d3b2455a437c5fcae659214a
EsinShadrach <esinshadrachrafe@gmail.com>
2023-10-23 02:00:13 +0100

    Edited Readme

--- app/resturant/toggleButton.tsx
+++ app/resturant/toggleButton.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useSidebarState } from "~/context/sidebar-context";
 
 export function ToggleButton() {
@@ -8,7 +7,7 @@ export function ToggleButton() {
     <button
       ref={buttonRef}
       onClick={handleToggle}
-      className="p-2 shadow-xl bg-inherit rounded-md space-y-2 py-2.5 transition-all duration-300 hover:opacity-40 active:scale-95 sm:hidden"
+      className="p-2 shadow-xl bg-inherit rounded-md space-y-2 py-2.5 transition-all duration-300 hover:opacity-40 active:scale-95 md:hidden"
     >
       <div className="w-6 h-0.5 bg-slate-900 rounded-full" />
       <div className="w-4 h-0.5 bg-slate-900 rounded-full" />
